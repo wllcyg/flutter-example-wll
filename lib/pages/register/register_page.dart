cea836357ae88b9376271173158953a423c49730
wll <90327500+wllcyg@users.noreply.github.com>
2026-04-09 07:06:17 +0800

    Update comment for codeController in register_page.dart

--- lib/pages/register/register_page.dart
+++ lib/pages/register/register_page.dart
@@ -16,7 +16,7 @@ class RegisterPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final emailController = useTextEditingController();
-    // final codeController = useTextEditingController(); // test1
+    // final codeController = useTextEditingController(); // test11
     final passwordController = useTextEditingController();
 
     return Scaffold(
